e9c6530f5e757d68ebcdefb79f41a4b85c8a2d56
Richard Lopes <richard.lopes@myob.com>
2015-05-16 08:45:06 +0000

    Experiment with motors

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ board.on('ready', function() {
   // Test board with a LED plugged on pin 13
   var led = new five.Led(13);
   led.blink(500);
+
+  var right = new five.Motor({pin: 9});
+  board.repl.inject({motor: right});
+  right.start(100);
+
+  var left = new five.Motor(8);
+  left.start(100);
 });
